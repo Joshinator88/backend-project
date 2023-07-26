09b89b1f0b14e3fb97a9b96c75e7405f0a721802
Joshua Bruijn <joshuaraatig@hotmail.com>
2023-07-26 23:34:24 +0200

    added my project

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<nav>`
+    <div class="logo"></div>
+    <!-- <img src="logo.png" alt="logo van chats"> -->
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Login</a></li>
+        <li><a href="">Register</a></li>
+    </ul>
+</nav>
+    
+    <form method="post">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username">
+        
+        <label for="password">Password</label>
+        <input type="password" name="Wachtwoord" id="wachtwoord">
+
+        <input type="submit" name="submit_login" id="submit_login" value="Login">
+    </form>
+
+</body>
+</html>
